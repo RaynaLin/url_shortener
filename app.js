2c28946fe810aed78acd397e679376e78c304a56
RaynaLin <lunavis0829@gmail.com>
2023-01-10 18:59:31 +0800

    feat: add hbs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 const db = mongoose.connection
 const app = express()
 const port = 3000
@@ -10,18 +11,20 @@ if (process.env.NODE_ENV !== 'production') {
 }
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // db status
 db.on('error', () => {
   console.log('mongodb error!')
 })
-
 db.once('open', () => {
   console.log('mongodb connected!')
 })
 
 
 app.get('/', (req, res) => {
-  res.send('hi!')
+  res.render('index')
 })
 
 app.listen(port, () => {
