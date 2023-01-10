d511e44c944e07d40654adbdcf284b7982388fd0
RaynaLin <lunavis0829@gmail.com>
2023-01-10 17:39:44 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const db = mongoose.connection
 const app = express()
 const port = 3000
+
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// db status
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 app.get('/', (req, res) => {
   res.send('hi!')
