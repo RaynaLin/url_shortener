5120d430544dde0aa2f8c4487084e36304f40746
RaynaLin <lunavis0829@gmail.com>
2023-01-11 22:26:49 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,80 +1,19 @@
-// 可以新增back to index的功能
-// 還未做create錯誤程式碼
-// 還未做shortener URL，error.hbs
-// 美化
-
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const Url = require('./models/urls')
-const randomUrl = require('./randomNum')
 const bodyParser = require('body-parser')
-const db = mongoose.connection
+const routes = require('./routes')
 const app = express()
 const port = 3000
-const routes = require('./routes')
+
+require('./config/mongoose')
 
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
-
-// db status
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-
-// app.get('/', (req, res) => {
-//   res.render('index')
-// })
-
-// // create
-// app.post('/', (req, res) => {
-//   if (!req.body.url) return res.redirect("/")
-//   const shortURL = randomUrl(5)
-
-//   Url.findOne({ origin: req.body.url })
-//     .lean()
-//     .then(data => {
-//       if (!data) {
-//         return Url.create({ origin: req.body.url, short: shortURL })
-//       }
-//       return data
-//     })
-//     .then(data => {
-//       res.render('success', { shortURL: data.short })
-//     })
-//     .catch(err => console.log(err))
-// })
-
-
-// // shortener URL
-// app.get('/:shortURL', (req, res) => {
-//   const shortURL = req.params.shortURL
-//   Url.findOne({ short: shortURL })
-//     .then(data => {
-//       if (!data) {
-//         return res.render('error', {
-//           ErrorMsg: "Can't found the URL:",
-//           ErrorURL: req.headers.host + "/" + shortURL
-//         })
-//       }
-
-//       res.redirect(data.origin)
-//     })
-//     .catch(err => console.log(err))
-// })
-
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const db = mongoose.connection
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -6,10 +7,11 @@ if (process.env.NODE_ENV !== 'production') {
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error!')
 })
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+
+module.exports = db
